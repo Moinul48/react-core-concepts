57296173d9780c23466e2a890675439e3134de29
Moinul48 <mhmoinul@gmail.com>
2021-02-26 18:57:59 +0600

    lecture 32.4

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person= {
+    name: "Dr. Mahfuz",
+    job: "Singer",
+  }
+  var person2= {
+    name: "Eva Rahman",
+    job: "Kokil Konthi",
+  }
+  var style = {
+    color: "red",
+    backgroundColor: "yellow",
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <p style={{fontSize:'40px'}}>I am a React Person</p>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="" style={style}>My Heading: {person.name+" "+person.job}</h1>
+        <h3 style={{backgroundColor:'cyan', color:'yellow'}}>Singer: {person2.name + " " + person2.job}</h3>
+        <p>My first react paragraph</p> */}
+       
       </header>
     </div>
   );
